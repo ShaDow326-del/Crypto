17f283211b3d7c193b064bd465efa47d13c07299
ShaDow326-del <iruhchidi@gmail.com>
2025-12-02 19:59:46 +0100

    Add files via upload

--- utils/erc20Abi.js
+++ utils/erc20Abi.js
@@ -2,7 +2,7 @@ const ABI = [
   "function name() view returns (string)",
   "function symbol() view returns (string)",
   "function decimals() view returns (uint8)",
-  "function balanceOf(address) view returns (uint256)"
+  "function balanceOf(address) view returns (uint256)",
 ];
 
 export default ABI;

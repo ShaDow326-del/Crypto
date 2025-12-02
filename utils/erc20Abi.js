65e33f56047e6556c6d4c3ed423b9de5a4b65997
ShaDow326-del <iruhchidi@gmail.com>
2025-12-02 15:32:13 +0100

    Add ERC20 ABI definition to erc20Abi.js

---
+++ utils/erc20Abi.js
@@ -0,0 +1,8 @@
+const ABI = [
+  "function name() view returns (string)",
+  "function symbol() view returns (string)",
+  "function decimals() view returns (uint8)",
+  "function balanceOf(address) view returns (uint256)"
+];
+
+export default ABI;

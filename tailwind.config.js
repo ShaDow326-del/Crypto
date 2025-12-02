07ecfa4545560056b3abe567d91567a1ddfca835
ShaDow326-del <iruhchidi@gmail.com>
2025-12-02 19:47:13 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: []
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        glass: 'rgba(255,255,255,0.06)'
+      }
+    }
+  },
+  plugins: [],
 };
